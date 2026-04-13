dd8b8bcd3fae3aa2835baac2ff24a02e57cc7356
Fellipe Mendonça <fs.mendonca@outlook.com>
2026-04-13 14:46:29 -0300

    fix: Remover criação de Policy para o Gitactions

--- infra/iam.tf
+++ infra/iam.tf
@@ -147,35 +147,3 @@ resource "aws_iam_role_policy_attachment" "step_function_attach" {
   role       = aws_iam_role.step_function_role.name
   policy_arn = aws_iam_policy.step_function_policy.arn
 }
-
-# Policy do GitHub
-# Permite criar e alterar infraestrutura
-
-resource "aws_iam_policy" "github_actions_policy" {
-  name = "earaujo-github-actions-policy"
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Action = [
-          "s3:*",
-          "dynamodb:*",
-          "lambda:*",
-          "glue:*",
-          "states:*",
-          "iam:*"
-        ]
-        Resource = "*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "github_attach" {
-  role       = aws_iam_role.github_actions_role.name
-  policy_arn = aws_iam_policy.github_actions_policy.arn
-}
-
-#
