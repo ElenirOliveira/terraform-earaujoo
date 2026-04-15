4bf3090aa5829383dbc9980d2fe61b34b7cd0e47
ElenirOliveira <eleniraraujo3@gmail.com>
2026-04-15 16:20:02 -0300

    fix(lambda): remove pysus e usa download direto datasus

--- src/lambda/index.py
+++ src/lambda/index.py
@@ -1,70 +1,47 @@
 
-# 1. IMPORTS
-
 import json
 import boto3
+import requests
 import os
-import tempfile
-
-import pandas as pd
-from pysus.online_data.SIH import download
-
-
-# 2. HANDLER PRINCIPAL
 
 def lambda_handler(event, context):
 
-   
-    # 3. CONFIGURAÇÃO
-  
+    # ============================
+    # CONFIG
+    # ============================
     bucket_name = os.environ.get("BUCKET_NAME")
 
-    # Cliente S3
     s3 = boto3.client("s3")
 
-    
-    # 4. DOWNLOAD DO DATASUS (PARQUET DIRETO )
-   
-    # Isso já baixa e salva parquet automaticamente no ambiente
-    files = download(
-        states="SP",
-        years=2020,
-        months=1,
-        groups="RD"
-    )
+    url = "https://datasus.saude.gov.br/wp-content/ftp/sihsus/202001_/Dados/RDSP2001.dbc"
 
-    # Caminho do arquivo gerado pelo pysus
-    local_parquet_path = "/tmp/RDSP2001.parquet"
+    # ============================
+    # DOWNLOAD
+    # ============================
+    response = requests.get(url, stream=True, timeout=60)
 
-    # Copiar do diretório padrão para /tmp (Lambda só escreve aqui)
-    original_path = files[0].path
+    if response.status_code != 200:
+        raise Exception("Erro ao baixar arquivo")
 
-    df = pd.read_parquet(original_path)
+    file_path = "/tmp/RDSP2001.dbc"
 
+    with open(file_path, "wb") as f:
+        for chunk in response.iter_content(chunk_size=1024):
+            if chunk:
+                f.write(chunk)
 
-    # 5. SALVAR TEMPORÁRIO (LAMBDA)
-  
-    df.to_parquet(local_parquet_path, index=False)
+    # ============================
+    # UPLOAD S3 (LZ)
+    # ============================
+    s3_key = "LZ/datasus/internacoes/year=2020/month=01/RDSP2001.dbc"
 
-   
-    # 6. DEFINIR CAMINHO NO S3 (LZ)
+    with open(file_path, "rb") as f:
+        s3.upload_fileobj(f, bucket_name, s3_key)
 
-    file_name = "LZ/datasus/internacoes/year=2020/month=01/RDSP2001.parquet"
-
-
-    # 7. UPLOAD PARA S3
- 
-    with open(local_parquet_path, "rb") as f:
-        s3.upload_fileobj(f, bucket_name, file_name)
-
-    
-    # 8. RETORNO
-   
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "message": "Parquet salvo na LZ com sucesso 🚀",
-            "bucket": bucket_name,
-            "path": file_name
+            "message": "Arquivo enviado para LZ",
+            "path": s3_key
         })
     }
